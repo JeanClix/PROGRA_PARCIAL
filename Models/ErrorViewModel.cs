4876c38b880f53bdc772029fbf5d4b2fb277aa94
JeanClix <jp10.02.2003@gmail.com>
2024-10-04 18:42:40 -0500

    firstcommit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PROGRA_PARCIAL.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
